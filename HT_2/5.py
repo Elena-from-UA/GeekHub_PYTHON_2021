dfd48117606308f4b1689daf8f09a6a5a4a19d9f
Elena Dashchenko <94141548+Elena-from-UA@users.noreply.github.com>
2021-11-21 14:54:54 +0200

    Update 5.py

--- HT_2/5.py
+++ HT_2/5.py
@@ -2,11 +2,13 @@
 Написати скрипт, який залишить в словнику тільки пари із унікальними значеннями
 (дублікати значень - видалити). Словник для роботи захардкодити свій.'''
 
-userDict = {'1':'test', '2':'python', '3':'hometask', '4':'test', '5':'hometask'}
+userDict = {'1':'test', '2':[1,2,3], '3':'hometask', '4':'test', '5':'hometask', '6': (1,2,5)}}
 values = set()
 newDict = {}
 
 for k,v in userDict.items():
+    if type(v) == list:
+            v = tuple(v)
     if v not in values:
         newDict.update({k: v})
         values.add(v)
