93281052ac664ad1c1ff35bf08c4f52086378b88
Elena Dashchenko <94141548+Elena-from-UA@users.noreply.github.com>
2021-11-30 23:01:37 +0200

    Create 6.py

---
+++ HT_5/6.py
@@ -0,0 +1,15 @@
+''' Task 6
+Всі ви знаєте таку функцію як <range>. Напишіть свою реалізацію цієї функції.
+P.S. Повинен вертатись генератор.'''
+
+def func_range(start,stop):
+    i = start
+    while i < stop:
+        yield i
+        i += 1
+
+for i in func_range(1,9):
+    print(i)
+    
+#check next(n)    
+n = func_range(1,9)
