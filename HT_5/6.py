435439e05ba1d0df37eb81e8e92aebde56016f29
Elena Dashchenko <94141548+Elena-from-UA@users.noreply.github.com>
2021-12-02 22:10:34 +0200

    Update 6.py

--- HT_5/6.py
+++ HT_5/6.py
@@ -2,12 +2,42 @@
 Всі ви знаєте таку функцію як <range>. Напишіть свою реалізацію цієї функції.
 P.S. Повинен вертатись генератор.'''
 
-def func_range(start,stop,step=1):
-    i = start
-    while i < stop:
-        yield i
-        i += step
+def func_range(*args):     
+    if len(args) == 1:
+        start = 0
+        stop = args[0]
+        step = 1
+        while start < stop:
+            yield start
+            start += step
+    elif len(args) == 2:
+        start = args[0]
+        stop = args[1]
+        step = 1
+        while start < stop:
+            yield start
+            start += step
+    elif len(args) == 3:
+        if args[2] > 0:
+            start = args[0]
+            stop = args[1]
+            step = args[2]
+            while start < stop:
+                yield start
+                start += step
+        elif args[2] < 0:
+            start = args[0]
+            stop = args[1]
+            step = args[2]
+            while stop > start:
+                yield stop
+                stop -= step*(-1)
+        else:
+            raise ValueError('Step cannot to be equal zero')
+    else:
+        raise TypeError('Range expected at most 3 arguments')
+               
 
-for i in func_range(1,20,3):
+for i in func_range(1,20,-2):
     print(i)
     
