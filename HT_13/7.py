322438429e9cea2e0ea88de25155c79d42db57cb
Elena Dashchenko <94141548+Elena-from-UA@users.noreply.github.com>
2022-01-04 21:56:41 +0200

    Create 7.py

---
+++ HT_13/7.py
@@ -0,0 +1,14 @@
+''' Task 7
+7. Створити пустий клас, який називається Thing.
+Потім створіть об'єкт example цього класу. Виведіть типи зазначених об'єктів.'''
+
+class Thing:
+    pass
+
+example = Thing
+
+print(example)
+print(Thing)
+
+print(type(example))
+print(type(Thing()))
